612a32e0edd1ff413d9c8f018ee801fc24f65515
kotoyuuko <zero@ero.cat>
2018-11-08 14:15:19 +0000

    basic layouts

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            {{ env('APP_COPYRIGHT') }}
+            Powered by ChaChaX.
+        </p>
+
+        <p class="pull-right">Made with <span class="love">❤</span> by <a href="https://artifact.moe" target="_blank">kotoyuuko</a>.</p>
+    </div>
+</footer>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title', '') - {{ env('APP_NAME') }}</title>
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app" class="{{ route_class() }}-page">
+        @include('layouts._header')
+        <div class="container">
+            @yield('content')
+        </div>
+        @include('layouts._footer')
+    </div>
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
