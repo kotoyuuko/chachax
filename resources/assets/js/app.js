28f725ef1c29744e7ef590e411cfd967501cf379
kotoyuuko <zero@ero.cat>
2018-11-09 06:19:19 +0000

    hover to show navbar dropdown and some fix for navbar

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,16 +7,74 @@
 
 require('./bootstrap');
 
+/*
 window.Vue = require('vue');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
 const app = new Vue({
     el: '#app'
+});
+*/
+
+$(function () {
+    var p = document.createElement('p'),
+        styles = {
+            width: '100px',
+            height: '1px',
+            overflowY: 'scroll'
+        },
+        i;
+    for (i in styles) p.style[i] = styles[i];
+    document.body.appendChild(p);
+    window.scrollbarWidth = p.offsetWidth - p.clientWidth;
+    document.body.removeChild(p);
+
+    window.fullWidth = function () {
+        return document.body.offsetWidth + window.scrollbarWidth;
+    };
+    
+    $('.dropdown').on('show.bs.dropdown', function () {
+        if (window.fullWidth() >= 768) {
+            $(this).find('.dropdown-menu')
+                .first()
+                .stop(true, true)
+                .show(200);
+        }
+    });
+    $('.dropdown').on('hide.bs.dropdown', function () {
+        if (window.fullWidth() >= 768) {
+            $(this).find('.dropdown-menu')
+                .first()
+                .stop(true, true)
+                .hide(200, function () {
+                    $(this).removeAttr('style');
+                });
+        }
+    });
+    $('.dropdown').hover(function () {
+        if (window.fullWidth() >= 768) {
+            $(this).trigger('show.bs.dropdown')
+                .addClass('open')
+                .trigger('shown.bs.dropdown')
+                .find('.dropdown-toggle')
+                .attr('aria-expanded', 'true');
+        }
+    }, function () {
+        if (window.fullWidth() >= 768) {
+            $(this).trigger('hide.bs.dropdown')
+                .removeClass('open')
+                .trigger('hidden.bs.dropdown')
+                .find('.dropdown-toggle')
+                .attr('aria-expanded', 'false');
+        }
+    });
+    $('.dropdown .dropdown-toggle').on('click', function(e) {
+        if (window.fullWidth() >= 768) {
+            if ($(this).attr('aria-expanded') == 'true') {
+                e.preventDefault();
+                return false;
+            }
+        }
+    });
 });
