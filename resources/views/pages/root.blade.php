fd8faef62f9f7c584877eb554cd7a5d2a16135d4
kotoyuuko <zero@ero.cat>
2018-11-10 17:24:19 +0000

    root page

--- resources/views/pages/root.blade.php
+++ resources/views/pages/root.blade.php
@@ -2,5 +2,14 @@
 @section('title', '首页')
 
 @section('content')
-    <h1>这里是首页</h1>
+    <section id="banner">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h2>给你更好的网络体验</h2>
+                    <a href="{{ route('login') }}" class="btn btn-primary btn-lg">现在体验</a>
+                </div>
+            </div>
+        </div>
+    </section>
 @stop
