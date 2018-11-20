7602e10098bc251563de467c0d9de342535e6f61
kotoyuuko <zero@ero.cat>
2018-11-20 15:12:38 +0000

    traffic package model

---
+++ database/migrations/2018_11_20_230829_create_packages_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('packages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->decimal('traffic', 64, 4);
+            $table->decimal('price', 16, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('packages');
+    }
+}
